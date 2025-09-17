ddff0738755a945e715fb02d7e48c5700a154c68
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-17 00:02:15 -0400

    Add utility class with a function to apply a SHA256 algorithm to a string and return it

---
+++ src/main/java/com/nickbell/StringUtil.java
@@ -0,0 +1,31 @@
+package com.nickbell;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class StringUtil {
+
+    public static String applySha256( String input ) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hast = digest.digest( input.getBytes( "UTF-8" ) ); // TODO: Address this warning.
+            StringBuffer hexString = new StringBuffer();    // Contains the hash as hexadecimal
+
+            // TODO: Revisit warning
+            for ( int i =0; i < hast.length; i++ ) {
+                String hex = Integer.toHexString(0xff & hast[i]);
+
+                if ( hex.length() == 1 ) hexString.append('0');
+
+                hexString.append( hex );
+            }
+
+            System.out.println( "hexString : " + hexString.toString() );
+            return hexString.toString();
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+    }
+}
