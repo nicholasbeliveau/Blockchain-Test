0a6a7b0c516c9cbd1cbfe004870545e759c78109
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-17 00:05:35 -0400

    Add calculateHash function to Block class

--- src/main/java/com/nickbell/Block.java
+++ src/main/java/com/nickbell/Block.java
@@ -9,6 +9,13 @@ public class Block {
     private String data;    // This could be any data
     private long timeStamp;
 
+    public String calculateHash() {
+
+        return StringUtil.applySha256(
+                previousHash + Long.toString(timeStamp) + data
+        );
+    }
+
     public Block( String data, String previousHash ) {
         this.data = data;
         this.previousHash = previousHash;
