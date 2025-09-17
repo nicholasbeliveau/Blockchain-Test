b3e6b748bf6572f8e6d997bffd96fec7e9b60ceb
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-17 11:18:07 -0400

    Use an ArrayList to represent the blockChain.

--- src/main/java/com/nickbell/Main.java
+++ src/main/java/com/nickbell/Main.java
@@ -1,15 +1,20 @@
 package com.nickbell;
 
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class Main {
+
+    public static ArrayList<Block> blockChain = new ArrayList<Block>();
+
     public static void main(String[] args) {
 
-        Block blockOne = new Block( "Initial Block", "0" );
-        System.out.println( "Block 1 : " + blockOne.hash );
+        blockChain.add( new Block("Initial Block", "0") );
+        blockChain.add( new Block("This is the second block", blockChain.get(blockChain.size()-1).hash) );
+        blockChain.add( new Block("Third block", blockChain.get(blockChain.size()-1).hash) );
 
-        Block blockTwo = new Block( "This is the second block", blockOne.hash );
-        System.out.println( "Block 2 : " + blockTwo.hash );
-
-        Block blockThree = new Block( "Initial Block", blockTwo.hash );
-        System.out.println( "Block 3 : " + blockThree.hash );
+        String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
+        System.out.println(blockChainJson);
     }
 }
