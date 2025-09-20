111a2c5a6203829becb4aeeebe9daa809648dd87
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-20 14:32:40 -0400

    Move original blockchain test to its own funciton and add a transaction test to main.

--- src/main/java/com/nickbell/Main.java
+++ src/main/java/com/nickbell/Main.java
@@ -2,14 +2,16 @@ package com.nickbell;
 
 import com.google.gson.GsonBuilder;
 
+import java.security.Security;
 import java.util.ArrayList;
 
 public class Main {
 
     public static ArrayList<Block> blockChain = new ArrayList<Block>();
-
     // Keeping this at 5.  Tested up to 7 and saw results anywhere from 13 seconds to 143 seconds.  5 works quickly
     public static int difficulty = 5;
+    public static Wallet walletOne;
+    public static Wallet walletTwo;
 
     public static Boolean validChain() {
 
@@ -41,8 +43,7 @@ public class Main {
         System.out.println( "Should be false : " + validChain() );
     }
 
-    public static void main(String[] args) {
-
+    public void miningTest() {
         blockChain.add( new Block("Initial Block", "0") );
         System.out.println( "Mining block 1" );
         blockChain.get(0).mineBlock(difficulty);
@@ -64,5 +65,26 @@ public class Main {
         else {
             System.out.println( "Chain is Invalid" );
         }
+    }
+
+    public static void main(String[] args) {
+
+        Security.addProvider( new org.bouncycastle.jce.provider.BouncyCastleProvider() );
+
+        walletOne = new Wallet();
+        walletTwo = new Wallet();
+
+        // Test the keys
+        System.out.println( "Private and public keys:" );
+        System.out.println( StringUtil.getStringFromKey( walletOne.privateKey ) );
+        System.out.println( StringUtil.getStringFromKey( walletTwo.publicKey ) );
+
+        // Create test transaction
+        Transaction transaction = new Transaction( walletOne.publicKey, walletTwo.publicKey, 5, null );
+        transaction.generateSignature( walletOne.privateKey );
+
+        // Verify the signature
+        System.out.println( "Is signature verified" );
+        System.out.println( transaction.verifySignature() );
     }
 }
