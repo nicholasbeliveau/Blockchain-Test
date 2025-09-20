944ede7ad650387e2bb1b807f06a0ab1c7bc3ca1
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-20 16:42:31 -0400

    Fill out Input and Output classes to determin ownership of coins.

--- src/main/java/com/nickbell/TransactionInput.java
+++ src/main/java/com/nickbell/TransactionInput.java
@@ -1,4 +1,11 @@
 package com.nickbell;
 
 public class TransactionInput {
+
+    public String transactionOutputId;
+    public TransactionOutput UTXO;      // Contains the unspent transaction output
+
+    public TransactionInput( String transactionOutputId ) {
+        this.transactionOutputId = transactionOutputId;
+    }
 }
--- src/main/java/com/nickbell/TransactionOutput.java
+++ src/main/java/com/nickbell/TransactionOutput.java
@@ -1,4 +1,22 @@
 package com.nickbell;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
+
+    public String id;
+    public PublicKey recipient; // Owner of the coins
+    public float value;
+    public String parentTransactionId;
+
+    public TransactionOutput( PublicKey recipient, float value, String parentTransactionId ) {
+        this.recipient = recipient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256( StringUtil.getStringFromKey( recipient ) + Float.toString( value ) + parentTransactionId);
+    }
+
+    public boolean isMine( PublicKey publicKey ) {
+        return ( publicKey == recipient );
+    }
 }
