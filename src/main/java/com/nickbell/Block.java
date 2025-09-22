41b4cd7a6f4fd681ca130cba7cd205083c49cd91
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-09-21 21:40:14 -0400

    Add addTransaction funciton to Block and switch to using merkleRoot instead of data field.

--- src/main/java/com/nickbell/Block.java
+++ src/main/java/com/nickbell/Block.java
@@ -1,18 +1,20 @@
 package com.nickbell;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data;    // This could be any data
+    public String merkleRoot;
+    public ArrayList< Transaction > transactions = new ArrayList< Transaction >();
     private long timeStamp;
     private int nonce;
 
     public Block( String data, String previousHash ) {
-        this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
@@ -24,13 +26,16 @@ public class Block {
                   previousHash +
                         Long.toString(timeStamp) +
                         Integer.toString(nonce) +
-                        data
+                        merkleRoot
         );
     }
 
     public void mineBlock( int difficulty ) {
 
+        merkleRoot = StringUtil.getMerkleRoot( transactions );
         String target = new String( new char[difficulty]).replace('\0', '0');
+        // TODO: Take a look at hte below line.  Its added in the tutorial, but I don't see the funciton in the tutorial
+        //String target = StringUtil.getDifficultyString( difficulty );
         long startTime = System.nanoTime();
 
         while ( !hash.substring( 0, difficulty).equals(target) ) {
@@ -45,5 +50,20 @@ public class Block {
         System.out.println( "Mined in " + TimeUnit.SECONDS.convert( elapsedTime, TimeUnit.NANOSECONDS ) + " seconds." );
     }
 
+    public boolean addTransaction( Transaction transaction ) {
 
+        if ( transaction == null ) return false;
+
+        if ( !Objects.equals( previousHash, "0" ) ) {
+
+            if ( !transaction.processTransaction() ) {
+                System.out.println( "Transaction failed to process." );
+                return false;
+            }
+
+            transactions.add( transaction );
+            System.out.println( "Transaction added Successfully." );
+            return true;
+        }
+    }
 }
